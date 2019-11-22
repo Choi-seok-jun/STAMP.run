c263a627db74f4ca81bdded051a3a4f66538a58f
sungwoo1548 <swo1548@gmail.com>
2019-11-22 14:45:05 +0900

    min fixed by ksw

--- model_app/user.js
+++ model_app/user.js
@@ -2,43 +2,58 @@ const mongoose = require("mongoose");
 const Joi = require("@hapi/joi");
 const { Schema, model } = mongoose;
 
+/**
+ * 1. Schema로 구조 만든다
+ * 2. model로 collection의 document 접근 방법 설정
+ * 3. @hapi/joi 모듈을 이용해 입력값 검증을 한다
+ */
+
 const personalSchema = new Schema({
+  // 입력값
   name: String,
   email: { type: String, unique: true },
   phone_num: { type: String, unique: true },
+  // 나중에 채워지는 값
   admin: { type: Boolean, default: false },
   userAdd: { type: mongoose.Types.ObjectId, ref: "User" },
   emailAuth: { type: String, default: "" },
   emailCheck: { type: Boolean, default: false }
 });
-const userSchema = new Schema({
-  id: { type: String, unique: true },
-  password: String,
-  persAdd: { type: mongoose.Types.ObjectId, ref: "Personal" }
-});
 
 const Personal = model("Personal", personalSchema);
-const User = model("User", userSchema);
 
 function validatePersonal(personal) {
   const schema = Joi.object({
     name: Joi.string(),
     email: Joi.string().email(),
     phone_num: Joi.string(),
-    userAdd: Joi.array().items(Joi.string()),
+    admin:Joi.boolean(),
+    userAdd: Joi.string(),
     emailAuth: Joi.string(),
     emailCheck: Joi.string()
   });
   return schema.validate(personal);
 }
+
+
+const userSchema = new Schema({
+  id: { type: String, unique: true },
+  password: String,
+  persAdd: { type: mongoose.Types.ObjectId, ref: "Personal" }
+});
+
+const User = model("User", userSchema);
+
 function validateUser(user) {
   const schema = Joi.object({
     id: Joi.string(),
     password: Joi.string(),
-    persAdd: Joi.array().items(Joi.string())
+    persAdd: Joi.string(),
   });
   return schema.validate(user);
 }
+
+
 module.exports = {
   User,
   Personal,
--- router_app/user.js
+++ router_app/user.js
@@ -7,121 +7,127 @@ const { Personal, validatePersonal } = require("../model_app/user");
 const { jwtSecret } = require("../common_app/jwt_config");
 const wrapper = require("../common_app/wrapper");
 
-// 객체로 보내주는것은 객체로 받아야함 객체를 req.body에 담았어도 따로따로 받으려면 객체로 받아서 객체 담겨진것을 불러와야함.
-router.post(
-  "/join",
-  wrapper(async (req, res, next) => {
-    const { name, email, password, id, phone_num } = req.body;
-    if (validatePersonal({ name, email, phone_num }).error) {
-      //검증과정 통과 못하면
-      res.status(400).json({ result: false });
-      next();
-      return;
-    }
-    if (validateUser({ password, id }).error) {
-      //검증과정 통과 못하면
-      res.status(400).json({ result: false });
-      next();
-      return;
-    }
+// post : 회원가입
+router.post("/join", wrapper(async (req, res, next) => {
+  const { name, email, phone_num, id, password } = req.body;
 
-    const saltRound = 10;
-    const hashedPW = await bcrypt.hash(password, saltRound);
-    const personal = new Personal({
-      name,
-      email,
-      phone_num
-    });
-    const saveResult = await personal.save();
-    const user = new User({
-      password: hashedPW,
-      id,
-      persAdd: personal._id
-    });
-    const saveResult2 = await user.save();
+  if (validatePersonal({ name, email, phone_num }).error) {    //검증과정 통과 못하면
+    res.status(400).json({ result: false });
+    next();
+    return;
+  }
+  if (validateUser({ password, id }).error) {    //검증과정 통과 못하면
+    res.status(400).json({ result: false });
+    next();
+    return;
+  }
 
-    await Personal.updateOne(
-      { _id: personal._id },
-      { $set: { userAdd: user._id } }
-      //User를 만들고 난뒤에 업데이트를 하는데  personal 이 만들어질떄 user에있는_id가  userAdd 로 들어가는데
-      //그이후에 personal 에 있는 userAdd와 User 에 있는 원본이랑 비교해서 같으면
-      //personal이 만들어질때 생긴 _id가 userAdd로 들어간다.
-      // await Personal.updateOne(user._id).toString();
+  // 비번 hash 
+  const saltRound = 10;
+  const hashedPW = await bcrypt.hash(password, saltRound);
+
+  // 새로운 [개인정보] 생성 및 DB에 저장
+  const new_personal = new Personal({ name, email, phone_num });
+  await new_personal.save();
+
+  // 새로운 [계정정보] 생성 및 DB에 저장
+  const new_user = new User({ password: hashedPW, id, persAdd: new_personal._id });
+  await new_user.save();
+
+  // 새로운 [계정정보]의 _id를 [개인정보에 연결]
+  await Personal.updateOne(
+    { _id: new_personal._id },            // 검색 조건
+    { $set: { userAdd: new_user._id } }   // 업데이트 내용
+  );
+
+  res.json({ result: true, msg: "회원가입 완료" });
+  next();
+}));
+
+// post : 로그인
+router.post("/login", wrapper(async (req, res, next) => {
+  // 사용자 입력값
+  const { id, password } = req.body;
+
+  // DB 조회
+  const user = await User.findOne({ id: id });
+
+  // DB 조회 결과가 없으면, 더 진행하지 않고 next()호출 후, return으로 이 router를 벗어남.
+  if (!user) {
+    res.json({ result: false, msg: "ID 없음" });
+    next();
+    return;
+  }
+
+  // DB 조회 결과가 있으면, 비번 비교 후 token 발행
+  const userInputPassword = req.body.password;
+  const dbPassword = user.password;
+  const comparePassword = await bcrypt.compare(userInputPassword, dbPassword);
+
+  if (comparePassword) {   // 비밀번호가 맞는경우 토큰을 만들어줌!
+    const token = jwt.sign(
+      { userAdd: user._id },   // 토큰 주요 내용
+      jwtSecret,          // 암호문
+      { expiresIn: "10m" } // 유효기간
     );
-    res.json({ result: true });
-    next();
-  })
-);
 
-router.post(
-  "/login",
-  wrapper(async (req, res, next) => {
-    const { id, password } = req.body;
-    const user = await User.findOne({ id: id });
-    //뒤의 이메일은 사용자가 입력한것 앞의 것은 데이터베이스에 들어있는 값
-    if (!user) {
-      res.json({ msg: "아이디가 없습니다", result: false });
-      next();
-      return;
-    }
-    const result = await bcrypt.compare(password, user.password);
-    //처음껀 입력한 비밀번호 , 2번째껀 DB에 들어있는 해쉬된 비밀번호 맞는것을 bcrypt가 비교해줌
-    if (result) {
-      //비밀번호가 맞는경우 토큰을 만들어줌!
-      const token = jwt.sign(
-        {
-          id: user._id
-        },
-        jwtSecret,
-        { expiresIn: "5m" }
-      );
-      res.json({ result: true, token });
-      next();
-    } else {
-      res.json({ msg: "비밀번호가 맞지않습니다.", result: false });
-      next();
-    }
-  })
-);
-router.get(
-  "/id",
-  wrapper(async (req, res, next) => {
-    const id = req.query.id;
-    console.log(id);
-    const rs = await User.findOne({ id });
-    if (rs) {
-      res.json({ result: false }); //중복
-    } else {
-      res.json({ result: true });
-    }
+    res.json({ result: true, msg: "로그인 되었습니다.", token }); // front로 token전달
     next();
-  })
-);
-router.get(
-  "/phone_num",
-  wrapper(async (req, res, next) => {
-    const phone_num = req.query.phone_num;
-    const rs = await Personal.findOne({ phone_num });
-    if (rs) {
-      res.json({ result: false });
-    } else {
-      res.json({ result: true });
-    }
+
+  } else { // 비밀번호가 다를 경우
+    res.json({ result: false, msg: "비밀번호가 맞지않습니다." });
     next();
-  })
-);
-router.get(
-  "/email",
-  wrapper(async (req, res, next) => {
-    const email = req.query.email;
-    const rs = await Personal.findOne({ email });
-    if (rs) {
-      res.json({ result: false });
-    } else {
-      res.json({ result: true });
-    }
+  }
+}));
+
+// get : id 중복검사
+router.get("/id", wrapper(async (req, res, next) => {
+  const id = req.query.id;
+  if (!id) { // 입력값이 없을 때, 중복검사 결과가 null 이므로, 사용가능이 되는 것을 방지
+    res.json({ error: "Auth", msg: "잘못된 접근입니다." });
     next();
-  })
-);
+    return;
+  }
+  const db_result = await User.findOne({ id });
+  if (db_result) { // db조회 결과가 있으면 = 이미 있다는 뜻
+    res.json({ result: false, msg: "중복" });
+  } else {
+    res.json({ result: true, msg: "사용가능" });
+  }
+
+  next();
+}));
+// get : phone_num 중복검사
+router.get("/phone_num", wrapper(async (req, res, next) => {
+  const phone_num = req.query.phone_num;
+  if (!phone_num) { // 입력값이 없을 때, 중복검사 결과가 null 이므로, 사용가능이 되는 것을 방지
+    res.json({ error: "Auth", msg: "잘못된 접근입니다." });
+    next();
+    return;
+  }
+  const db_result = await Personal.findOne({ phone_num });
+  if (db_result) { // db조회 결과가 있으면 = 이미 있다는 뜻
+    res.json({ result: false, msg: "중복" });
+  } else {
+    res.json({ result: true, msg: "사용가능" });
+  }
+  next();
+}));
+// get : email 중복검사
+router.get("/email", wrapper(async (req, res, next) => {
+  const email = req.query.email;
+  if (!email) { // 입력값이 없을 때, 중복검사 결과가 null 이므로, 사용가능이 되는 것을 방지
+    res.json({ error: "Auth", msg: "잘못된 접근입니다." });
+    next();
+    return;
+  }
+  const db_result = await Personal.findOne({ email });
+  if (db_result) { // db조회 결과가 있으면 = 이미 있다는 뜻
+    res.json({ result: false, msg: "중복" });
+  } else {
+    res.json({ result: true, msg: "사용가능" });
+  }
+  next();
+}));
 
 module.exports = router;
