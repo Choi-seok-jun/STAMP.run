1d78a9a149f85c2b965fea8e7fb97b1a814f3ca7
Choi-seok-jun <50814945+Choi-seok-jun@users.noreply.github.com>
2019-11-22 16:16:02 +0900

    fix

--- model_app/emailHtml.js
+++ model_app/emailHtml.js
@@ -3,7 +3,7 @@ const emailContents = (num) => (`
     <table align="center" border="1" cellpadding="0" cellspacing="0" width="600">
     <tr>
         <td>
-        <img src="../public/인트로.png" alt="Creating Email Magic" width="600 " height="600"
+        <img src="../images/인트로.png" alt="Creating Email Magic" width="600 " height="600"
         style="display: block;" />
         </td>
     </tr>
